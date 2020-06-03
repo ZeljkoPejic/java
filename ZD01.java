2b61efc8f1974bf762d0efec890df8d518b7b3b2
Zeljko Pejic <zeljkopejic@yahoo.com>
2020-06-03 19:59:06 +0200

    java kod

---
+++ ZD01.java
@@ -0,0 +1,29 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class ZD01 {
+
+	
+	public static void main(String[] args) {
+		
+		int prviBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi prvi broj"));
+		int drugiBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi broj"));
+		int treciBroj = Integer.parseInt(JOptionPane.showInputDialog("Unesi treci broj"));
+		
+		int broj = 0;
+		
+		if(prviBroj >= drugiBroj ) {
+			broj = prviBroj;
+		}
+		else {
+			broj = drugiBroj;
+		}
+		if(treciBroj >= broj) {
+				broj = treciBroj;
+				
+			}
+		
+		System.out.println("Najveci broj je "+broj);
+	}
+}
